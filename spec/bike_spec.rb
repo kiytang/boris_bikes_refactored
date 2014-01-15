7b5949a3e32eac5e5285b0939bbb8cb366aade1a
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-15 11:18:08 +0000

    added spec files

---
+++ spec/bike_spec.rb
@@ -0,0 +1,15 @@
+#link to the Bike class
+require_relative "../lib/bike"
+
+# we are describing the functionality of a specific class, Bike
+describe Bike do
+
+	#this is a specific feature(behaviour)
+	#that we expect to be present
+	it "should not be broken after we create it" do
+		bike = Bike.new #initialise a new object of Bike class
+		# expect an instance of this calss to have
+		# a method "broken?" that should return false
+		expect(bike).not_to be_broken
+	end
+end
