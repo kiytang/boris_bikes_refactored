d14ae3503014af5aa1e8f7fa77014fa15974e114
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-17 14:51:48 +0000

    extracting docling station methods into a module

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,33 +1,14 @@
+#load BikeContainer
+require_relative 'bike_container'
+
 class DockingStation
-	DEFAULT_CAPACITY = 10
+
+	#the below gives us all the methods that are contained
+	#in BikeCotainer. As it is the same, we remove as hown below:
+	include BikeContainer
 
 	def initialize(options = {})
-		@capacity = options.fetch(:capacity) || DEFAULT_CAPACITY
-		@bikes = []
-	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		#if capacity is reached, raise and exception
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end	
-
-	def full?
-		bike_count == @capacity
-
-	end
-
-	def available_bikes
-			@bikes.reject {|bike| bike.broken?}
+		self.capacity = options.fetch(:capacity, capacity)
 	end
 
 end
-
