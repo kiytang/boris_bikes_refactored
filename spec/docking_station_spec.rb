9fbfcb715dfbd2db21b46139d7615213d463e96a
Kiy Hong Tang <kiyhong@gmail.com>
2014-01-17 14:53:06 +0000

    extracting docling station methods into a module

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,49 +1,10 @@
-require_relative "../lib/docking_station"
+require "./lib/docking_station"
 
 describe DockingStation do
 
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
+	let(:station) { DockingStation.new(:capacity => 123) }
 
-	it "should accept a bike" do
-		# bike = Bike.new
-		# station = DockingStation.new
-		#we expect the station to have 0 bikes
-		expect(station.bike_count).to eq(0)
-		#let's dock a bike into the station
-		station.dock(bike)
-		#now we expect the station to have 1 bike
-		expect(station.bike_count).to eq(1)
-	end
-
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq(0)
-	end
-
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		fill_station station
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it is full" do
-		fill_station station
-		expect(lambda { station.dock(bike) } ).to raise_error(RuntimeError)
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq([working_bike])
-	end
-
-
-	def fill_station(station)
-		20.times { station.dock(Bike.new) }
-	end
-
+	it "should allow setting default capacity on initialising" do
+		expect(station.capacity).to eq(123)
+	end	
 end
